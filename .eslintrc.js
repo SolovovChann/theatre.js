eb660dfb9f35759c869db080a1d2537ae466a3cb
SolovovChann <solnik2001@gmail.com>
2022-07-09 20:25:27 +0500

    feature: basic eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'require-jsdoc': ['warn', {
+      'require': {
+        'FunctionDeclaration': true,
+        'MethodDefinition': false,
+        'ClassDeclaration': false,
+        'ArrowFunctionExpression': false,
+        'FunctionExpression': false,
+      },
+    }],
+  },
+};
